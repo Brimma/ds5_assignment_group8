9f3c98c5b79a57ddd1410d66c2c814929c61ba7d
Brimma <bram.bleumink@gmail.com>
2024-09-16 21:22:17 +0200

    _

--- Excersice_1.py
+++ Excersice_1.py
@@ -1,18 +1,18 @@
 # Read a csv file and transform this to a list
-    file_path = input("Enter the path to the CSV file: ")
-    records = []
-    with open(file_path, 'r') as file:
-        csv_reader = csv.DictReader(file)
-        for row in csv_reader:
-            records.append(row)
+file_path = input("Enter the path to the CSV file: ")
+records = []
+with open(file_path, 'r') as file:
+    csv_reader = csv.DictReader(file)
+    for row in csv_reader:
+        records.append(row)
 
 
 # Compute the average grade
-    total = sum(float(record['Grade']) for record in records)
-    average = total / len(records)
+total = sum(float(record['Grade']) for record in records)
+average = total / len(records)
 
 # Filters the grades on being greater than 80
-    filtered_records = [record for record in records if float(record['Grade']) >= 80.0]
+filtered_records = [record for record in records if float(record['Grade']) >= 80.0]
 
 
 print(f"Average Grade: {average}")
@@ -21,7 +21,7 @@ print("Student Report")
 print("--------------")
 
 # Prints the name and grade of every student that got a grade higher than 80
-    for record in filtered_records:
-        print(f"Name: {record['Name']}")
-        print(f"Grade: {record['Grade']}")
-        print("--------------------")
+for record in filtered_records:
+    print(f"Name: {record['Name']}")
+    print(f"Grade: {record['Grade']}")
+    print("--------------------")
